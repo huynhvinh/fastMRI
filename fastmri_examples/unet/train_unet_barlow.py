f32ef732335298b81efff22214e88af678b0b37a
karan-sarkar <karan@sarkarfamily.org>
2021-05-03 08:21:16 -0400

    Remove barlow nargs

--- fastmri_examples/unet/train_unet_barlow.py
+++ fastmri_examples/unet/train_unet_barlow.py
@@ -120,7 +120,6 @@ def build_args():
     )
     parser.add_argument(
                 "--proportion",
-        nargs="+",
         default=0.1,
         type=float,
         help="Proportion of label data",
